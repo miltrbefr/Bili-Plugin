0a6f2de8e59a5aa9ccb1b1e7d23f3d8158af9d3b
NenNen_CN <1514664085@qq.com>
2025-04-15 20:47:50 +0800

    整合功能

--- apps/QQBot.js
+++ apps/QQBot.js
@@ -47,7 +47,77 @@ let attempts = 0
 const maxAttempts = 250
 const delay = 200
 const checkAdapters = async () => {
-    const QQBot1 = Bot.adapter.find(adapter => adapter.version === 'qq-group-bot v11.45.14');
+    let QQBot = Bot.adapter.find(adapter => adapter.version === 'qq-group-bot v11.45.14')
+    if (QQBotconfig) QQBot = Bot.adapter.find(adapter => adapter.name === 'QQBot')
+    const ICQQ = Bot.adapter.find(adapter => adapter.name === 'ICQQ')
+    const setupICQQ = (adapter) => {
+        adapter.sendMsg = async function(id, pick, msg, ...args) {
+            const rets = {
+                message_id: [],
+                data: [],
+                error: []
+            }
+            try {
+                const processMessages = async () => {
+                    const processed = {
+                        buttonMsgs: [],
+                        normalMsgs: [],
+                        rawMsgs: Array.isArray(msg) ? [...msg] : [msg]
+                    }
+                    for (const item of processed.rawMsgs) {
+                        const type = item?.type;
+                        if (type === 'button') {
+                            processed.buttonMsgs.push({
+                                type: "button",
+                                appid: this.markdown_appid,
+                                content: {
+                                    rows: this.makeButtons(id, pick, item.data)
+                                },
+                            })
+                        } else {
+                            processed.normalMsgs.push(item);
+                        }
+                    }
+                    let normalMsgs = await this.makeMsg(id, pick, processed.normalMsgs);
+                    if (normalMsgs.length === 0) {
+                        return {
+                            messages: processed.buttonMsgs,
+                        }
+                    }
+                    if (Array.isArray(normalMsgs) && normalMsgs.length > 0 && Array.isArray(normalMsgs[0]))
+                        normalMsgs = normalMsgs[0]
+
+                    return {
+                        messages: configs.sendbutton ? [[...normalMsgs], ...processed.buttonMsgs] : normalMsgs,
+                    }
+                }
+                let { messages } = await processMessages()
+                const sendMsg = async () => {
+                    for (const i of messages) try {
+                        Bot.makeLog("debug", ["发送消息", i], id)
+                        const ret = await pick.sendMsg(i, ...args)
+                        Bot.makeLog("debug", ["发送消息返回", ret], id)
+                        rets.data.push(ret)
+                        if (ret.message_id)
+                            rets.message_id.push(ret.message_id)
+                    } catch (err) {
+                        rets.error.push(err)
+                        return false
+                    }
+                }
+                if (await sendMsg() === false) {
+                    messages = await this.makeMsg(id, pick,
+                        [await Bot.makeForwardMsg([{ message: messages?.[0] }])])
+                    await sendMsg()
+                }
+                return rets.data.length === 1 ? rets.data[0] : rets
+            } catch (error) {
+                Bot.makeLog("error", ["消息发送错误", messages, error], id)
+                rets.error.push(error)
+                return rets
+            }
+        }
+    }
     const setupQQBot = (adapter) => {
         adapter.makeMsg = async function(data, msg) {
             const sendType = ['audio', 'image', 'video', 'file']
@@ -199,9 +269,10 @@ const checkAdapters = async () => {
         }
     }
     const handleAdapters = () => {
-        if (QQBot1) setupQQBot(QQBot1)
+        if (QQBot) setupQQBot(QQBot)
+        if (ICQQ && configs.sendbutton) setupICQQ(ICQQ)
     }
-    if (QQBot1) {
+    if (QQBot && ICQQ) {
         handleAdapters()
         return
     }
@@ -213,4 +284,4 @@ const checkAdapters = async () => {
     await new Promise(resolve => setTimeout(resolve, delay));
     await checkAdapters()
 }
-if (isTRSS && QQBotconfig && configs.QQBotsendlink) checkAdapters()
+if (isTRSS && (QQBotconfig && configs.QQBotsendlink || configs.sendbutton)) checkAdapters()
