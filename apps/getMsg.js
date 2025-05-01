cfdab007dc69713bed51552bad7dfcb91cfef802
NenNen_CN <1514664085@qq.com>
2025-05-02 00:00:57 +0800

    细节优化

--- apps/getMsg.js
+++ apps/getMsg.js
@@ -1,46 +1,49 @@
 export class getMsg extends plugin {
-    constructor() {
+  constructor() {
       super({
-        name: "取消息",
-        event: "message.group",
-        priority: 1000,
-        rule: [{
-          reg: "^#?get$",
-          fnc: "get"
-        }]
+          name: "取消息",
+          event: "message.group",
+          priority: 1000,
+          rule: [{
+              reg: "^#?get$",
+              fnc: "get"
+          }]
       })
-    }
-  
-    async get(e) {
+  }
+
+  async get(e) {
       if (!['ICQQ', 'OneBotv11'].includes(e?.bot?.adapter?.name)) return false
       let id = e.reply_id ? e.reply_id : e.source.seq
       if (!id) return e.reply("请回复要get的消息")
       let isSeq = e?.bot?.adapter?.name === 'ICQQ'
       const data = await Packet.getMsg(
-        e,
-        id,
-        isSeq
+          e,
+          id,
+          isSeq
       )
       const forwardNodes = []
       const msg = []
-      const target = data?.["3"]?.["6"]?.["3"]?.["1"]?.["2"]
-      if (target && Array.isArray(target)) {
-        msg.push("pb(elem):")
-        msg.push("可使用rjPacket.Elem(this.e, { } )进行发送pb消息")
-        for (const i of target) {
-          msg.push(JSON.stringify(i,Packet.replacer, 2))
-        }
+      let target = data?.["3"]?.["6"]?.["3"]?.["1"]?.["2"]
+      let g = false
+      if (!Array.isArray(target)) {
+          target = [target]
+          g = true
+      }
+      msg.push("pb(elem):")
+      if (!g) msg.push("可使用rjPacket.Elem(this.e, { } )进行发送pb消息")
+      for (const i of target) {
+          msg.push(JSON.stringify(i, Packet.replacer, 2))
       }
       msg.push("pb(raw):")
-      msg.push(JSON.stringify(data,Packet.replacer, 2))
+      msg.push(JSON.stringify(data, Packet.replacer, 2))
       for (const i of msg) {
-        forwardNodes.push({
-            user_id: e.user_id,
-            nickname: e.sender.nickname,
-            message: i
-        })
+          forwardNodes.push({
+              user_id: e.user_id,
+              nickname: e.sender.nickname,
+              message: i
+          })
       }
       const forwardMessage = await Bot.makeForwardMsg(forwardNodes)
       e.reply(forwardMessage)
-    }
+  }
 }
